59b93c0a3eca88bbc9d0206be08f0225f99685d6
EvgenijRyabko <jeniaryabko@bk.ru>
2023-05-22 02:42:07 +0300

    upload

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -24,7 +24,87 @@ const getByID = async (req, res) => {
   }
 };
 
+const checkDirectory = async (path) => {
+  try {
+    await fs.promises.access(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+const writeProfileImage = async (path, idUser, buffer) => {
+  // создать файл
+  await fs.promises.writeFile(path, Buffer.from(buffer), 'UTF-8');
+  // создать запись в бд
+  await knexConnection('users').where('id', idUser).update('profile-img', path);
+};
+
+const writeImages = async (path, idUser, buffer) => {
+  // создать файл
+  await fs.promises.writeFile(path, Buffer.from(buffer), 'UTF-8');
+  // создать запись в бд
+  await knexConnection('users').where('id', idUser).update('images', path);
+};
+
+const uploadImagesByUser = async (req, res) => {
+  const { id: idUser } = req.params;
+  try {
+    if (!idUser) throw 'Параметр idTerminal не найден';
+
+    const path = `uploadFiles/${idUser}/`;
+
+    const checkPath = await checkDirectory(path);
+
+    if (!checkPath) await fs.promises.mkdir(path, { recursive: true });
+
+    const arrayFiles = req.body.files.file;
+
+    if (Array.isArray(arrayFiles)) {
+      for (const iterator of arrayFiles) {
+        await writeImages(path + iterator.name, idUser, iterator.data.data);
+      }
+    } else await writeImages(path + arrayFiles.name, idUser, arrayFiles.data.data);
+
+    res.status(200).json([]);
+  } catch (e) {
+    const error = new Error(e);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const uploadProfileImageByUser = async (req, res) => {
+  const { id: idUser } = req.params;
+  try {
+    if (!idUser) throw 'Параметр idTerminal не найден';
+
+    const path = `uploadFiles/${idUser}/profile/`;
+
+    const checkPath = await checkDirectory(path);
+
+    if (!checkPath) await fs.promises.mkdir(path, { recursive: true });
+
+    const arrayFiles = req.body.files.file;
+
+    console.log(arrayFiles);
+
+    if (Array.isArray(arrayFiles)) {
+      for (const iterator of arrayFiles) {
+        await writeProfileImage(path + iterator.name, idUser, iterator.data.data);
+      }
+    } else await writeProfileImage(path + arrayFiles.name, idUser, arrayFiles.data.data);
+
+    res.status(200).json([]);
+  } catch (e) {
+    console.log(e);
+    const error = new Error(e);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getUsers,
   getByID,
+  uploadImagesByUser,
+  uploadProfileImageByUser,
 };
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,8 +1,19 @@
-const { getUsers, getByID } = require('../../controllers/users');
+const {
+  getUsers,
+  getByID,
+  uploadProfileImageByUser,
+  uploadImagesByUser,
+} = require('../../controllers/users');
 
 module.exports = (app, url, ...args) => {
   // Получить пользователей
   app.get(`${url}/getAll`, [...args], getUsers);
+
+  // Закинуть изображение профиля
+  app.post(`${url}/uploadProfile/:id`, [...args], uploadProfileImageByUser);
+
+  // Закинуть изображения на страницу
+  app.post(`${url}/upload/:id`, [...args], uploadImagesByUser);
 
   // Получить пользователя по id
   app.get(`${url}/:id`, [...args], getByID);
